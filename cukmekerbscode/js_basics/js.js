3b6e62d035642f0c131b93895cd9d8d577ae2912
vincens2005 <bokheadpictures@gmail.com>
2018-04-22 11:17:31 -0700

    Create js.js

---
+++ cukmekerbscode/js_basics/js.js
@@ -0,0 +1,11 @@
+var answer = prompt("do you like waffles?" , "Yes, they are the best!!!" );
+alert("Your answer was: "+answer);
+if(answer.includes("Yes") || answer.includes("yes")){
+alert("You are cool becaause you like waffl;es");
+}
+else if(answer.includes("maybe") || answer.includes("Maybe")){
+alert("you might be coll because maybe yoou like waffles")
+}
+else{
+alert("You are not coll because you dont like waffles");
+};
